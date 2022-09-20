be53f1ed26395f1c342f8ff006e2712e9b4a81fe
Sonia Flores Zúñiga <zunigafloressonia105@gmail.com>
2022-09-20 11:17:56 -0500

    Carpeta routes

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const messagesRouter = require('./routes/messages')
 
 class Server {
     constructor(){
         this.app = express()
+        this.paths = {
+            messages: "/api/v1/messages"
+        }
         this.routes()
     }
 
     routes() {
-        this.app.get('/', (req, res) => {
-            res.send('Hello Word')
-        })
+        //this.app.get('/', (req, res) => {
+        // res.send('Hello Word')
+        //  }) //end point
+      this.app.use(this.paths.messages, messagesRouter)
     }
 
     listen(){
