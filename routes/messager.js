0872283fd0f4ae0fe0fce7a0a8a7a6a2f83a9f32
Sonia Flores Zúñiga <zunigafloressonia105@gmail.com>
2022-09-23 11:08:41 -0500

    agregar post, put y delete

--- routes/messager.js
+++ routes/messager.js
@@ -1,10 +1,14 @@
 const {Router} = require ("express")
 const router = Router()
-const{rootMessage, hiMessage, byeMessage} = require ('../controllers/messages')
+const{rootMessage, hiMessage, byeMessage, postMessage, putMessage, deleteMessage} = require ('../controllers/messages')
 
 router.get('/', rootMessage) //end point
 router.get('/hi', hiMessage) //end point
 router.get('/bye', byeMessage)//end point
+
+router.post('/', postMessage)
+router.put('/', putMessage)
+router.delete('/', deleteMessage)
 
 
 module.exports = router
