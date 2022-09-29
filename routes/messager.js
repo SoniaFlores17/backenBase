746f3d023cac5c92e5c7d70cee0320692bcbae45
Sonia Flores Zúñiga <zunigafloressonia105@gmail.com>
2022-09-29 10:38:36 -0500

    Parametros URI

--- routes/messager.js
+++ routes/messager.js
@@ -3,7 +3,8 @@ const router = Router()
 const{rootMessage, hiMessage, byeMessage, postMessage, putMessage, deleteMessage} = require ('../controllers/messages')
 
 router.get('/', rootMessage) //end point
-router.get('/hi', hiMessage) //end point
+//router.get('/hi', hiMessage) //end point
+router.get('/hi/:name', hiMessage)
 router.get('/bye', byeMessage)//end point
 
 router.post('/', postMessage)
