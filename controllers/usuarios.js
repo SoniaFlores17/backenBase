35a8c78331aecd9008cbaa96bd51f4392e19547e
Sonia Flores Zúñiga <zunigafloressonia105@gmail.com>
2022-10-27 10:58:41 -0500

    Actualizar datos

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -162,6 +162,78 @@ const getUsers = async (req=request, res= response)=>{
         }
     
     }
-module.exports = {getUsers, getUserByID, deleteUserByID, ddUser}
+
+
+
+    const updateUserByUsuario = async (req=request, res= response)=>{
+        const{
+            Usuario, 
+            Nombre,
+            Apellidos,
+            Edad,
+            Genero, 
+            Contrasena, 
+            Fecha_Nacimiento = '1900-01-01',
+ 
+
+        }= req.body
+
+        if (
+            !Nombre ||
+            !Apellidos ||
+            !Edad ||
+            !Contrasena 
+
+        ) {
+            res.status(400).json({msg: "Falta información del usuario"})
+            return
+        }
+        let conn;
+
+        try{
+            conn=await pool.getConnection()
+
+            const [user] = await conn.query(`SELECT Usuario, Nombre, Apellidos, Edad, Genero, Fecha_Nacimiento
+             FROM Usuarios WHERE Usuario = '${Usuario}'`)
+
+            if (!user) {
+                res.status(403).json({msg: `El usuario ${Usuario} no se encuentra registrado.`})
+                return
+            }
+    
+            const {affectedRows} =await conn.query(`
+                UPDATE Usuarios SET
+                    Nombre ='${Nombre || user.Nombre}',
+                    Apellidos = '${Apellidos || user.Apellidos}',
+                    Edad = '${Edad || user.Edad}',
+                    Genero = '${Genero || user.Genero }', 
+                    Fecha_Nacimiento = '${Fecha_Nacimiento}'
+                WHERE Usuario = '${Usuario}'
+                
+            `,(error)=>{throw new error})
+
+
+
+            //console.log(userDeleted)
+         if(affectedRows === 0){
+               res.status(404).json({msg:`No se pudo actualizar el registro del usuario  ${Usuario}`})
+              return
+
+            }
+            res.json({msg: `El usuario con ID ${Usuario} se actualizó satisfactoriamente.`})
+        }catch(error){
+            console.log(error)
+            res.status(500).json({error})
+        }finally{
+            if(conn){
+                conn.end()
+    
+            }
+        }
+    
+    }
+
+    
+module.exports = {getUsers, getUserByID, deleteUserByID, ddUser, updateUserByUsuario}
 
 //Genero ? chalala : nochalala
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,14 +1,21 @@
 const {Router} = require("express")
-const {getUsers, getUserByID, deleteUserByID, ddUser} = require("../controllers/usuarios")
+const {getUsers, getUserByID, deleteUserByID, ddUser, updateUserByUsuario, } = require("../controllers/usuarios")
 const router = Router()
 
 // http://localhost:4000/api/v1/usuarios?id=
 
+///GET///
 router.get("/", getUsers)
 router.get("/id/:id", getUserByID)
 
+///POST///
 router.post("/", ddUser)
 
+///PUT///
+router.put("/", updateUserByUsuario)
+
+
+///DELETE///
 router.delete("/", deleteUserByID)
 
 module.exports = router
